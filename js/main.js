5014961a6dfc835ac27f3c17ff5a134db7d4d256
Sarvar <sarvar95yil@gmail.com>
2020-12-26 15:05:38 +0300

    adaptive works and some adds

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function () {
 
+
+  const downButton = document.getElementById('down-button');
   // document.event.preventDefault();
       
 var mySwiper = new Swiper('#specialties ',  {
@@ -19,7 +21,7 @@ var mySwiper = new Swiper('#specialties ',  {
 var mySwiper = new Swiper('#galerie', {
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
-  slidesPerView: 5.3,
+  slidesPerView: window.innerWidth/360,
   spaceBetween: 0,
   loop: true,
   // mousewheel: {
@@ -36,7 +38,6 @@ var mySwiper = new Swiper('#galerie', {
 });
     
   let tabsItem = $('.menu-navigation-items');
-  console.log(tabsItem)
   tabsItem.on('click', function(event) {
       console.log($(this).attr('href'))
       event.preventDefault();
@@ -46,34 +47,7 @@ var mySwiper = new Swiper('#galerie', {
       $(activeContent).toggleClass('visible');          
   });
   
-  
+  // downButton.addEventListener('click', href = "#about")
 });
 
 
-
-function doStuff(){
-  landscape = window.orientation? window.orientation=='landscape' : true;
-
-  if(landscape && window.innerWidth<1368){
-    var mySwiper = new Swiper('#galerie', {
-      watchSlidesProgress: true,
-      watchSlidesVisibility: true,
-      slidesPerView: 4.0001,
-      spaceBetween: 0,
-      loop: true,
-      // mousewheel: {
-      //   invert: true,
-      // },
-     
-      autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-      speed: 1400
-    
-      
-    });
-  }
-}
-
-doStuff();
