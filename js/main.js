8806b96c923b58ce8e9ac18c270eab8833ddd713
Sarvar <sarvar95yil@gmail.com>
2020-12-26 11:22:35 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ var mySwiper = new Swiper('#galerie', {
   watchSlidesVisibility: true,
   slidesPerView: 5.3,
   spaceBetween: 0,
-  loop: false,
+  loop: true,
   // mousewheel: {
   //   invert: true,
   // },
@@ -48,3 +48,32 @@ var mySwiper = new Swiper('#galerie', {
   
   
 });
+
+
+
+function doStuff(){
+  landscape = window.orientation? window.orientation=='landscape' : true;
+
+  if(landscape && window.innerWidth<1368){
+    var mySwiper = new Swiper('#galerie', {
+      watchSlidesProgress: true,
+      watchSlidesVisibility: true,
+      slidesPerView: 4.0001,
+      spaceBetween: 0,
+      loop: true,
+      // mousewheel: {
+      //   invert: true,
+      // },
+     
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+      speed: 1400
+    
+      
+    });
+  }
+}
+
+doStuff();
